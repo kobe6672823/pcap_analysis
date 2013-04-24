f4c7cc7d5150652a5b914077e5bcd0930b52f592
hm <kobe6672823@qq.com>
2013-04-24 19:38:53 +0800

    finish: tcp; modify header_len in ip; update: pro&sol

--- code/ip.py
+++ code/ip.py
@@ -29,7 +29,7 @@ class Ip(Protocol):
         
         #please look up the structure of the IPV4 Protocol to get the detail of the fields below
         self.version = None
-        self.header_len = None
+        self.header_len = None #in bytes
         self.type_of_service = None
         self.total_len = None
         self.id = None
@@ -50,7 +50,7 @@ class Ip(Protocol):
         """a method to decode the infomation in the packet_data, and fill in the fields above"""
         
         self.version = int(data_to_hex_str(self.packet[0])[2])
-        self.header_len = int(data_to_hex_str(self.packet[0])[3])
+        self.header_len = int(data_to_hex_str(self.packet[0])[3]) * 4
         self.type_of_service = data_to_hex_str(self.packet[1:2])
         self.total_len = int(data_to_hex_str(self.packet[2:4]), 16)
         self.id = data_to_hex_str(self.packet[4:6])
@@ -90,8 +90,8 @@ class Ip(Protocol):
         self.dst = str(ord(self.packet[16])) + '.' + str(ord(self.packet[17])) + '.' + \
             str(ord(self.packet[18])) + '.' + str(ord(self.packet[19]))
             
-        if (self.header_len > 5):
-            self.opt_paddings = self.packet[20 : (self.header_len * 4)]
+        if (self.header_len > 20):
+            self.opt_paddings = self.packet[20 : (self.header_len)]
     #endof def
     
     def print_info(self):
