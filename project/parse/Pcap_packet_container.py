5ab02c947d810f9864806cf94376c1cde6927094
hm <kobe6672823@qq.com>
2013-05-06 10:56:23 +0800

    finish: cut the pcap file into tcp streams

--- project/parse/Pcap_packet_container.py
+++ project/parse/Pcap_packet_container.py
@@ -6,6 +6,7 @@ from ethernet import *
 from ip import *
 from tcp import *
 from Pcap_packet import *
+from tcp_stream import *
 
 class Pcap_packet_container():
     """a class to contain the packets in a pcap file"""
@@ -19,6 +20,7 @@ class Pcap_packet_container():
         self.packet_headers, \
         self.raw_packets = rd_pcap(self.pcap_file_name)
         self.pcap_packets = []
+        self.tcp_stream_container = []
     #endof def
     
     def parse(self):
@@ -46,9 +48,30 @@ class Pcap_packet_container():
             
             pcap_packet.top_layer = 3
             pcap_packet.tcp = Tcp(pcap_packet.ip.packet[pcap_packet.ip.header_len: ])
+            
+            self.add_pkt_into_tcp_stream(pcap_packet, number)
         #endof for
     #endof def
     
+    def add_pkt_into_tcp_stream(self, pcap_packet, num):
+        """a method to add a pcap_packet into a tcp stream, if it does not belong to any existing tcp stream, 
+        create a new one"""
+        
+        socket_pair = set()
+        socket_pair.add((pcap_packet.ip.src, pcap_packet.tcp.src_port))
+        socket_pair.add((pcap_packet.ip.dst, pcap_packet.tcp.dst_port))
+        
+        for stream in self.tcp_stream_container:
+            if (socket_pair == stream.socket_pair):
+                stream.pcap_num_list.append(num)
+                break
+        else:
+            new_stream = Tcp_stream(pcap_packet.ip.src, pcap_packet.tcp.src_port, 
+                pcap_packet.ip.dst, pcap_packet.tcp.dst_port)
+            new_stream.pcap_num_list.append(num)
+            self.tcp_stream_container.append(new_stream)
+        
+        
     def print_info(self):
         """a method to print all the packets in a container to the stander output"""
         
